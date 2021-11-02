d930248e2cee6b394f9cba4d9b9f3f43c0ec7e53
HeonSeokCha <hs960829@gmail.com>
2021-11-02 21:30:25 +0900

    add Appbar.

--- app/src/main/java/com/chs/coutubemusic/MainActivity.kt
+++ app/src/main/java/com/chs/coutubemusic/MainActivity.kt
@@ -1,22 +1,32 @@
 package com.chs.coutubemusic
 
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ExitToApp
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.MailOutline
+import androidx.compose.material.icons.filled.PlayArrow
+import androidx.compose.material.icons.outlined.List
+import androidx.compose.material.icons.twotone.PlayArrow
+import androidx.compose.material.icons.twotone.Search
+import androidx.compose.material.icons.twotone.Share
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -26,6 +36,7 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import com.chs.coutubemusic.ui.theme.BottomBarColor
 import com.chs.coutubemusic.ui.theme.CoutubeMusicTheme
 import java.nio.file.Files.size
 
@@ -36,6 +47,9 @@ class MainActivity : ComponentActivity() {
             CoutubeMusicTheme {
                 val navController = rememberNavController()
                 Scaffold(
+                    topBar = {
+                        Appbar()
+                    },
                     bottomBar = {
                         BottomNavigationBar(
                             items = listOf(
@@ -47,12 +61,12 @@ class MainActivity : ComponentActivity() {
                                 BottomNavItem(
                                     name = "Explore",
                                     route = "explore",
-                                    icon = Icons.Default.ExitToApp
+                                    icon = ImageVector.vectorResource(id = R.drawable.ic_un_explore)
                                 ),
                                 BottomNavItem(
                                     name = "Library",
                                     route = "library",
-                                    icon = Icons.Default.MailOutline
+                                    icon = ImageVector.vectorResource(id = R.drawable.ic_un_library)
                                 )
                             ),
                             navController = navController,
@@ -66,6 +80,32 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+    }
+}
+
+@Composable
+fun Appbar() {
+    Column {
+        TopAppBar(title = {
+            Icon(imageVector = Icons.TwoTone.PlayArrow, contentDescription = null)
+            Text(text = "Music")
+        },
+            actions = {
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(imageVector = Icons.TwoTone.Share, contentDescription = null)
+                }
+
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(imageVector = Icons.TwoTone.Search, contentDescription = null)
+                }
+
+                IconButton(onClick = { /*TODO*/ }) {
+                    Image(
+                        painter = painterResource(id = R.drawable.test),
+                        contentDescription = null,
+                    )
+                }
+            })
     }
 }
 
@@ -94,7 +134,7 @@ fun BottomNavigationBar(
     val backStackEntry = navController.currentBackStackEntryAsState()
     BottomNavigation(
         modifier = modifier,
-        backgroundColor = Color.DarkGray,
+        backgroundColor = BottomBarColor,
         elevation = 5.dp
     ) {
         items.forEach { items ->
@@ -102,8 +142,8 @@ fun BottomNavigationBar(
             BottomNavigationItem(
                 selected = selected,
                 onClick = { onItemClick(items) },
-                selectedContentColor = Color.Green,
-                unselectedContentColor = Color.Gray,
+                selectedContentColor = Color.White,
+                unselectedContentColor = Color.White,
                 icon = {
                     Column(horizontalAlignment = CenterHorizontally) {
                         Icon(
@@ -149,5 +189,36 @@ fun LibraryScreen() {
         contentAlignment = Alignment.Center
     ) {
         Text(text = "Library Screen")
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewAppBar() {
+    CoutubeMusicTheme {
+        val navController = rememberNavController()
+        BottomNavigationBar(
+            items = listOf(
+                BottomNavItem(
+                    name = "Home",
+                    route = "home",
+                    icon = Icons.Default.Home
+                ),
+                BottomNavItem(
+                    name = "Explore",
+                    route = "explore",
+                    icon = ImageVector.vectorResource(id = R.drawable.ic_un_explore)
+                ),
+                BottomNavItem(
+                    name = "Library",
+                    route = "library",
+                    icon = ImageVector.vectorResource(id = R.drawable.ic_un_library)
+                )
+            ),
+            navController = navController,
+            onItemClick = {
+                navController.navigate(it.route)
+            }
+        )
     }
 }
