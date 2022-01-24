4b316cca5ef1734013bd1f8e3a12e883b181c8c2
HeonSeokCha <hs960829@gmail.com>
2022-01-24 23:45:11 +0900

    adding expanded music Player...#3

--- app/src/main/java/com/chs/coutubemusic/MainActivity.kt
+++ app/src/main/java/com/chs/coutubemusic/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.chs.coutubemusic
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.*
@@ -47,12 +48,12 @@ class MainActivity : ComponentActivity() {
 
             val scope = rememberCoroutineScope()
             val (canPop, setCanPop) = remember { mutableStateOf(false) }
-
             val scaffoldState = rememberBottomDrawerScaffoldState(
-              bottomDrawerState   = rememberBottomDrawerState(initialValue = BottomDrawerValue.Collapsed, drawerTopInset = 0)
+              bottomDrawerState   = rememberBottomDrawerState(
+                  initialValue = BottomDrawerValue.Collapsed,
+                  drawerTopInset = 0
+              )
             )
-
-            val bottomVisibility by remember { mutableStateOf(true) }
 
             val sheetToggle: () -> Unit = {
                 scope.launch {
@@ -127,9 +128,10 @@ class MainActivity : ComponentActivity() {
                             }
                             SheetCollapsed(
                                 isCollapsed = scaffoldState.bottomDrawerState.isCollapsed,
-                                currentFraction = 0f,
+                                currentFraction = scaffoldState.currentFraction,
                                 onSheetClick = sheetToggle
                             ) {
+                                Log.e("progress", scaffoldState.bottomDrawerState.progress.fraction.toString())
                                 if (scaffoldState.bottomDrawerState.isExpanded) {
                                     ExpandMusicPlayerScreen()
                                 } else {
@@ -231,16 +233,16 @@ fun BottomNavigationBar(
 }
 
 @ExperimentalMaterialApi
-val BottomSheetScaffoldState.currentFraction: Float
+val BottomDrawerScaffoldState.currentFraction: Float
     get() {
-        val fraction = bottomSheetState.progress.fraction
-        val targetValue = bottomSheetState.targetValue
-        val currentValue = bottomSheetState.currentValue
+        val fraction = bottomDrawerState.progress.fraction
+        val targetValue = bottomDrawerState.targetValue
+        val currentValue = bottomDrawerState.currentValue
 
         return when {
-            currentValue == BottomSheetValue.Collapsed && targetValue == BottomSheetValue.Collapsed -> 0f
-            currentValue == BottomSheetValue.Expanded && targetValue == BottomSheetValue.Expanded -> 1f
-            currentValue == BottomSheetValue.Collapsed && targetValue == BottomSheetValue.Expanded -> fraction
+            currentValue == BottomDrawerValue.Collapsed && targetValue == BottomDrawerValue.Collapsed -> 0f
+            currentValue == BottomDrawerValue.Expanded && targetValue == BottomDrawerValue.Expanded -> 1f
+            currentValue == BottomDrawerValue.Collapsed && targetValue == BottomDrawerValue.Expanded -> fraction
             else -> 1f - fraction
         }
     }
--- app/src/main/java/com/chs/coutubemusic/SheetContent.kt
+++ app/src/main/java/com/chs/coutubemusic/SheetContent.kt
@@ -1,19 +1,14 @@
 package com.chs.coutubemusic
 
-import android.util.Log
-import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.BottomNavigationItem
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.ContentScale
